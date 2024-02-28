8e3bf14fe97e384dc806f1ef7f029b873b8df529
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-02-28 17:15:05 -0300

    Update StoryMenuState.hx

--- source/meta/states/StoryMenuState.hx
+++ source/meta/states/StoryMenuState.hx
@@ -185,7 +185,9 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 		changeDifficulty();
-
+                #if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
 		super.create();
 	}
 
